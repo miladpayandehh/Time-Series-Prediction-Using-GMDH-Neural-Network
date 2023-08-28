10f5e982d495963fd734313fb708a942e874f20e
Milad Payandeh <127933124+miladpayandehh@users.noreply.github.com>
2023-08-28 17:05:33 +0330

    Add files via upload

---
+++ Time Series Prediction Using GMDH/app2.m
@@ -0,0 +1,59 @@
+clc;
+clear;
+close all;
+
+%% Load Data
+
+x = ice_dataset();
+x = cell2mat(x);
+
+Delays = [1 2 3 4 5];
+[Inputs, Targets] = CreateTimeSeriesData(x,Delays);
+
+nData = size(Inputs,2);
+Perm = randperm(nData);
+
+% Train Data
+pTrain = 0.9;
+nTrainData = round(pTrain*nData);
+TrainInd = Perm(1:nTrainData);
+TrainInputs = Inputs(:,TrainInd);
+TrainTargets = Targets(:,TrainInd);
+
+% Test Data
+pTest = 1 - pTrain;
+nTestData = nData - nTrainData;
+TestInd = Perm(nTrainData+1:end);
+TestInputs = Inputs(:,TestInd);
+TestTargets = Targets(:,TestInd);
+
+%% Create and Train GMDH Network
+
+params.MaxLayerNeurons = 30;   % Maximum Number of Neurons in a Layer
+params.MaxLayers = 5;          % Maximum Number of Layers
+params.alpha = 0;              % Selection Pressure
+params.pTrain = 0.7;           % Train Ratio
+gmdh = GMDH(params, TrainInputs, TrainTargets);
+
+%% Evaluate GMDH Network
+
+Outputs = ApplyGMDH(gmdh, Inputs);
+TrainOutputs = Outputs(:,TrainInd);
+TestOutputs = Outputs(:,TestInd);
+
+%% Show Results
+
+figure;
+PlotResults(TrainTargets, TrainOutputs, 'Train Data');
+
+figure;
+PlotResults(TestTargets, TestOutputs, 'Test Data');
+
+figure;
+PlotResults(Targets, Outputs, 'All Data');
+
+figure;
+plotregression(TrainTargets, TrainOutputs, 'Train Data', ...
+               TestTargets, TestOutputs, 'TestData', ...
+               Targets, Outputs, 'All Data');
+           
---
+++ Time Series Prediction Using GMDH/app4.m
@@ -0,0 +1,62 @@
+clc;
+clear;
+close all;
+
+%% Load Data
+
+x = oil_dataset();
+x = cell2mat(x);
+
+%Delays = [3 12 21 30];
+Delays = [1 2 3 11 19 27 35];
+[Inputs, Targets] = CreateTimeSeriesData(x,Delays);
+
+Targets = Targets(2,:);
+
+nData = size(Inputs,2);
+Perm = randperm(nData);
+
+% Train Data
+pTrain = 0.9;
+nTrainData = round(pTrain*nData);
+TrainInd = Perm(1:nTrainData);
+TrainInputs = Inputs(:,TrainInd);
+TrainTargets = Targets(:,TrainInd);
+
+% Test Data
+pTest = 1 - pTrain;
+nTestData = nData - nTrainData;
+TestInd = Perm(nTrainData+1:end);
+TestInputs = Inputs(:,TestInd);
+TestTargets = Targets(:,TestInd);
+
+%% Create and Train GMDH Network
+
+params.MaxLayerNeurons = 5;    % Maximum Number of Neurons in a Layer
+params.MaxLayers = 5;          % Maximum Number of Layers
+params.alpha = 0;              % Selection Pressure
+params.pTrain = 0.7;           % Train Ratio
+gmdh = GMDH(params, TrainInputs, TrainTargets);
+
+%% Evaluate GMDH Network
+
+Outputs = ApplyGMDH(gmdh, Inputs);
+TrainOutputs = Outputs(:,TrainInd);
+TestOutputs = Outputs(:,TestInd);
+
+%% Show Results
+
+figure;
+PlotResults(TrainTargets, TrainOutputs, 'Train Data');
+
+figure;
+PlotResults(TestTargets, TestOutputs, 'Test Data');
+
+figure;
+PlotResults(Targets, Outputs, 'All Data');
+
+figure;
+plotregression(TrainTargets, TrainOutputs, 'Train Data', ...
+               TestTargets, TestOutputs, 'TestData', ...
+               Targets, Outputs, 'All Data');
+           
